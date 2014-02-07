aba3833f690cec16c901eab9c12dc79dc8018a2d
SAntipov <santipov@korusconsulting.ru>
2014-02-07 16:36:05 +0400

    Добавлен 'скелет' для модуля Расписание

--- version.py
+++ version.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
-version = u'0.0.1'
-last_change_date = datetime(year=2014, month=02, day=6, hour=20,)
+version = u'0.0.2'
+last_change_date = datetime(year=2014, month=02, day=7, hour=16,)
