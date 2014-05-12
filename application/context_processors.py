43f7514299635fc0a86e96de8c1880d35b072a5c
SAntipov <santipov@korusconsulting.ru>
2014-05-12 11:53:56 +0400

    TMIS-58: изменение верхнего меню

--- application/context_processors.py
+++ application/context_processors.py
@@ -2,6 +2,7 @@
 from application.app import app
 from werkzeug.utils import import_string
 from datetime import datetime
+from flask.ext.login import current_user
 
 
 @app.context_processor
@@ -22,16 +23,10 @@ def print_subsystem():
 @app.context_processor
 def general_menu():
     menu_items = list()
-    blueprints = app.blueprints
-    for k, v in blueprints.items():
-        try:
-            config = import_string('%s.config' % import_string(v.import_name).__package__)
-        except ImportError, e:
-            print e
-        else:
-            if hasattr(config, 'RUS_NAME'):
-                menu_items.append(dict(module=v.name, name=config.RUS_NAME))
-            else:
-                menu_items.append(dict(module=v.name, name=v.name))
-
+    menu_items.append(dict(module='patients',
+                           link='patients.index',
+                           title=u'Обслуживание пациентов'))
+    menu_items.append(dict(module='schedule',
+                           link='schedule.person_schedule_monthview',
+                           title=u'Формирование графика врача'))
     return dict(main_menu=menu_items)
