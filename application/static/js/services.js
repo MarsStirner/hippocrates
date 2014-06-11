176894c9b154bfa509460455601e92eb04fddaa9
swod <roman.s3000@gmail.com>
2014-06-11 13:34:18 +0400

    Начало правок формы пациента - удалены вкладки. Удален angular-resource и обновлен код его использовавший.

--- application/static/js/services.js
+++ application/static/js/services.js
@@ -1,6 +1,120 @@
 'use strict';
 
 angular.module('WebMis20.services', []).
+    factory('WMClient', ['$http', '$q', '$rootScope',
+        function($http, $q, $rootScope) {
+            var WMClient = function(client_id) {
+                this.client_id = client_id;
+                this.reload();
+            };
+
+            WMClient.prototype.reload = function() {
+                var t = this;
+                $http.get(url_client_get, {
+                    params: {
+                        client_id: this.client_id
+                    }
+                }).success(function(data) {
+                    t.client_info = data.result.clientData;
+                    t.appointments = data.result.appointments;
+                    t.events = data.result.events;
+    //              $rootScope.$broadcast('client_loaded');
+                }).error(function(data, status) {
+                    $rootScope.$broadcast('load_error', {
+                        text: 'Ошибка при загрузке клиента ' + t.id,
+                        data: data,
+                        code: status,
+                        type: 'danger'
+                    });
+                    throw 'Error requesting Client, id = ' + t.client_id;
+                });
+            };
+
+            WMClient.prototype.save = function() {
+                var t = this;
+                var deferred = $q.defer();
+                $http.post(url_client_save, {
+                    client_info: this.client_info
+                }).success(function(value, headers) {
+                    deferred.resolve(value['result']);
+                }).error(function(httpResponse) {
+                    var r = httpResponse.data;
+                    var message = [r['result']['name'], ':\nНе заполнено поле ', r['result']['data']].join('');
+                    deferred.reject(message);
+                });
+                return deferred.promise;
+            };
+
+            WMClient.prototype.add_allergy = function() {
+                this.client_info['allergies'].push({
+                    'nameSubstance': '',
+                    'power': 0,
+                    'createDate': '',
+                    'deleted':0,
+                    'notes': '' });
+            };
+
+            WMClient.prototype.add_medicament = function() {
+                this.client_info['intolerances'].push({
+                    'nameMedicament': '',
+                    'power': 0,
+                    'createDate': '',
+                    'deleted':0,
+                    'notes': '' });
+            };
+
+            WMClient.prototype.add_identification = function() {
+                this.client_info['identifications'].push({
+                    'deleted': 0,
+                    'identifier': '',
+                    'accountingSystem_code': '',
+                    'checkDate': ''});
+            };
+
+            WMClient.prototype.add_contact = function() {
+                this.client_info['contacts'].push({
+                    'deleted': 0,
+                    'contactType_code': '',
+                    'contact': '',
+                    'notes': ''});
+            };
+
+            WMClient.prototype.add_blood = function () {
+                this.client_info['bloodHistory'].push({'bloodGroup_code': '',
+                    'bloodDate': '',
+                    'person_id': 0
+                });
+            };
+
+            WMClient.prototype.add_relation = function (entity) {
+                this.client_info[entity].push({'deleted': 0,
+                    'relativeType_name': '',
+                    'relativeType_code': '',
+                    'other_id': 0
+                });
+            };
+
+            WMClient.prototype.add_soc_status = function () {
+                this.client_info['socStatuses'].push({'deleted': 0,
+                    'classCode': '',
+                    'typeCode': '',
+                    'begDate': '',
+                    'endDate': ''
+                });
+            };
+
+            WMClient.prototype.delete_record = function(entity, record) {
+                if ('id' in record) {
+                    record['deleted'] = 1;
+                } else {
+                    var idx = this.client_info[entity].indexOf(record);
+                    this.client_info[entity].splice(idx, 1);
+                }
+            };
+
+            return WMClient;
+        }
+    ]).
     factory('WMEvent', ['$http', '$q', 'WMEventService',
         function($http, $q, WMEventService) {
             var WMEvent = function(event_id, client_id, ticket_id) {
