1ccefa9fed159be8f8fb781293ed48a3fcef2606
viruzzz-kun <viruzzz.soft@gmail.com>
2014-06-26 16:10:35 +0400

    Эта срань делает нас чуть-чуть более кросс-браузерными (из-за того, что метод элемента jQuery height нихрена не работает), но и всё равно глючит кое-где, но уже существенно менее предсказуемо.

--- application/static/js/webmis20.js
+++ application/static/js/webmis20.js
@@ -826,3 +826,22 @@ var aux = {
         return result
     }
 };
+if (!HTMLElement.prototype.hasOwnProperty('getOffsetRect')) {
+    HTMLElement.prototype.getOffsetRect = function () {
+        // (1)
+        var box = this.getBoundingClientRect();
+        // (2)
+        var body = document.body;
+        var docElem = document.documentElement;
+        // (3)
+        var scrollTop = window.pageYOffset || docElem.scrollTop || body.scrollTop;
+        var scrollLeft = window.pageXOffset || docElem.scrollLeft || body.scrollLeft;
+        // (4)
+        var clientTop = docElem.clientTop || body.clientTop || 0;
+        var clientLeft = docElem.clientLeft || body.clientLeft || 0;
+        // (5)
+        var top  = box.top +  scrollTop - clientTop;
+        var left = box.left + scrollLeft - clientLeft;
+        return { top: Math.round(top), left: Math.round(left) }
+    }
+}
