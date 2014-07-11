7c99c74f74312e56f48cbd4c01aa4917ffcfd22e
SAntipov <santipov@korusconsulting.ru>
2014-07-11 20:13:24 +0400

    В БД ФНКЦ используются свои хитрые коды полисов

--- application/lib/const.py
+++ application/lib/const.py
@@ -3,7 +3,7 @@
 # client
 ID_DOC_GROUP_CODE = '1'
 COMP_POLICY_CODES = ['cmiOld', 'cmiTmp', 'cmiCommonPaper', 'cmiCommonElectron', 'cmiUEC', 'cmiFnkcIndustrial',
-                     'cmiFnkcLocal', '1', '2']
+                     'cmiFnkcLocal', '0', '1']
 VOL_POLICY_CODES = ['vmi', '3']
 
 # event
