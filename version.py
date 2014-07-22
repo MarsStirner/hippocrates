4623b43bdc6cf6ee374f9045188c5b894a5e0fd8
SAntipov <santipov@korusconsulting.ru>
2014-07-22 18:29:53 +0400

    Fix: TMIS-193 обновления страницы предварительной записи

--- version.py
+++ version.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
-version = u'0.8.22'
-last_change_date = datetime(year=2014, month=07, day=22, hour=16,)
+version = u'0.8.23'
+last_change_date = datetime(year=2014, month=07, day=22, hour=18,)
