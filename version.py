f6728e17cedb6b11b6bdf8fd803a25fbf3fa9f59
Serge Antipov <antipov.serge@gmail.com>
2014-07-23 11:51:44 +0400

    Update version.py

--- version.py
+++ version.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
-version = u'0.8.23'
-last_change_date = datetime(year=2014, month=07, day=22, hour=18,)
+version = u'0.8.24'
+last_change_date = datetime(year=2014, month=07, day=23, hour=11,)
