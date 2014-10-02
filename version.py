7b61ca0d92056c1d0ad92a572a6928c58e83644a
plakrisenko <PLakrisenko@korusconsulting.ru>
2014-10-02 12:18:14 +0400

    v 1.0.18

--- version.py
+++ version.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
-version = u'1.0.17'
-last_change_date = datetime(year=2014, month=10, day=1, hour=14,)
+version = u'1.0.18'
+last_change_date = datetime(year=2014, month=10, day=2, hour=12,)
