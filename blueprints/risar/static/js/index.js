578645f59687408823d63109af8695dced85fc12
SAntipov <santipov@korusconsulting.ru>
2014-10-03 14:18:40 +0400

    RIMIS-17

--- blueprints/risar/static/js/index.js
+++ blueprints/risar/static/js/index.js
@@ -10,4 +10,11 @@ var IndexCtrl = function ($scope, RisarApi) {
         })
     });
     $scope.date = new Date();
+    $scope.declOfNum = function (number, titles){
+        if (number == undefined){
+            number = 0;
+        }
+        var cases = [2, 0, 1, 1, 1, 2];
+        return titles[ (number%100>4 && number%100<20)? 2 : cases[(number%10<5)?number%10:5] ];
+    }
 };
