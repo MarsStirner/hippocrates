f291333616f48a04b1b6d5734d7a7731753bae8e
swod <roman.s3000@gmail.com>
2014-10-07 15:33:29 +0400

    Добавлена возможность записывать и отменять записи пациентов к врачу из интерфейса графика врача на месяц, а также кнопка перехода в карту пациента рядом с временем номерка и фио пациента.
    Поиск пациента с результатами из интерфейса регистратора вынесен в отдельную
    директиву.

--- application/static/js/services/appointment_dialog.js
+++ application/static/js/services/appointment_dialog.js
@@ -44,45 +44,70 @@ angular.module('WebMis20.services.dialogs', ['WebMis20.services', 'ui.bootstrap'
             });
         },
         make: function (ticket, person, client_id, client_name) {
+            var size = client_id ? '' : 'lg';
             return $modal.open({
                 template:
                     '<div class="modal-header" xmlns="http://www.w3.org/1999/html">\
                         <button type="button" class="close" ng-click="cancel()">&times;</button>\
                         <h4 class="modal-title" id="myModalLabel">Записать на приём?</h4>\
                     </div>\
-                    <div class="modal-body">\
-                        <dl class="dl-horizontal novmargin">\
-                          <dt>Врач:</dt><dd>[[ person.name ]] ([[ person.speciality.name ]])</dd>\
-                          <dt>Время приёма:</dt>\
-                          <dd>\
-                            <span ng-if="ticket.attendance_type.code == \'planned\'">[[ ticket.begDateTime | asMomentFormat:\'HH:mm DD.MM.YY\' ]]</span>\
-                            <span ng-if="ticket.attendance_type.code == \'CITO\'"><strong>экстренно</strong></span>\
-                            <span ng-if="ticket.attendance_type.code == \'extra\'"><strong>сверх плана</strong></span>\
-                          </dd>\
-                          <dt ng-if="client_name">Пациент:</dt><dd ng-if="client_name">[[ client_name ]]</dd>\
-                        </dl>\
+                    <div class="modal-body modal-scrollable">\
+                        <div id="page1" ng-show="page === 0">\
+                            <wm-search-client client-id="client.client_id" query="client_query"\
+                                on-select="select_client(selected_client)"></wm-search-client>\
+                        </div>\
+                        <div id="page2" ng-show="page === 1">\
+                            <dl class="dl-horizontal novmargin">\
+                              <dt>Врач:</dt><dd>[[ person.name ]] ([[ person.speciality.name ]])</dd>\
+                              <dt>Время приёма:</dt>\
+                              <dd>\
+                                <span ng-if="ticket.attendance_type.code == \'planned\'">[[ ticket.begDateTime | asMomentFormat:\'HH:mm DD.MM.YY\' ]]</span>\
+                                <span ng-if="ticket.attendance_type.code == \'CITO\'"><strong>экстренно</strong></span>\
+                                <span ng-if="ticket.attendance_type.code == \'extra\'"><strong>сверх плана</strong></span>\
+                              </dd>\
+                              <dt ng-show="client_name">Пациент:</dt><dd ng-show="client_name">[[ client_name ]]</dd>\
+                            </dl>\
+                        </div>\
                     </div>\
                     <div class="modal-footer">\
-                        <button type="button" class="btn btn-success" ng-click="accept()">Записать</button>\
+                        <button type="button" class="btn btn-primary" ng-click="next_page()" ng-show="page === 0" ng-disabled="!client.client_id">Далее</button>\
+                        <button type="button" class="btn btn-default" ng-click="prev_page()" ng-show="page === 1">Назад</button>\
+                        <button type="button" class="btn btn-success" ng-click="accept()" ng-show="page === 1">Записать</button>\
                         <button type="button" class="btn btn-danger" ng-click="cancel()">Не записывать</button>\
                     </div>',
                 controller: function ($scope, $http, $modalInstance) {
                     $scope.ticket = ticket;
                     $scope.person = person;
+                    $scope.client = {
+                        client_id: client_id
+                    };
                     $scope.client_name = client_name;
+                    $scope.page = client_id ? 1 : 0;
+                    $scope.client_query = '';
+
+                    $scope.select_client = function (selected_client) {
+                        $scope.client_name = selected_client.info.full_name;
+                    };
+                    $scope.next_page = function () {
+                        $scope.page++;
+                    };
+                    $scope.prev_page = function () {
+                        $scope.page--;
+                    };
                     $scope.accept = function () {
-                        WMAppointment.make(ticket, client_id).success(function (data) {
+                        WMAppointment.make(ticket, $scope.client.client_id).success(function (data) {
                             $modalInstance.close(data);
                         }).error(function () {
                             $scope.error = {
                                 text: 'Ошибка при записи на приём'
-                            }
+                            };
                         });
                     };
                     $scope.cancel = function () {
                         $modalInstance.dismiss('cancel');
                     };
-                }
+                },
+                size: size
             });
         }
     }
