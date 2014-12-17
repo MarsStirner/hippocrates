a28c524d51a9c0a5f0c38bd6425180871c2a8067
swod <roman.s3000@gmail.com>
2014-12-17 16:47:36 +0300

    Version 1.1dev33

--- version.py
+++ version.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
-version = u'1.1dev32'
-last_change_date = datetime(year=2014, month=12, day=11, hour=19,)
+version = u'1.1dev33'
+last_change_date = datetime(year=2014, month=12, day=17, hour=17,)
