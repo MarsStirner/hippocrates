2fcdc1809774bf3af27e69635c56b088ab291bf6
plakrisenko <PLakrisenko@korusconsulting.ru>
2014-12-30 14:43:59 +0300

    RMIS-112 переход на вкладки с информацией о детях

--- blueprints/risar/static/js/epicrisis.js
+++ blueprints/risar/static/js/epicrisis.js
@@ -3,6 +3,18 @@
 var EpicrisisCtrl = function ($timeout, $scope, RisarApi) {
     var params = aux.getQueryParams(window.location.search);
     var event_id = $scope.event_id = params.event_id;
+
+    var open_tab = function (tab_name){
+        var prefix = "tab_";
+        tab_name = tab_name.replace(/\//, '');
+        $('.nav-pills a[href='+tab_name.replace(prefix,"")+']').tab('show');
+
+        // Change hash for page-reload
+        $('.nav-pills a').on('shown.bs.tab', function (e) {
+            window.location.hash = e.target.hash.replace("#", "#" + prefix);
+        })
+    }
+
     var reload_epicrisis = function () {
         RisarApi.chart.get(event_id)
         .then(function (data) {
@@ -12,15 +24,13 @@ var EpicrisisCtrl = function ($timeout, $scope, RisarApi) {
             }
             $timeout(function(){
                 var hash = document.location.hash;
-                var prefix = "tab_";
-                hash = hash.replace(/\//, '')
-                if (hash) {
-                    $('.nav-pills a[href='+hash.replace(prefix,"")+']').tab('show') ;
+                if (hash.match('child')){
+                    open_tab(hash);
                 }
             }, 0);
-
         })
     };
+
     $scope.save = function () {
         var model = $scope.chart.epicrisis;
         RisarApi.epicrisis.save($scope.chart.id, model)
@@ -37,16 +47,9 @@ var EpicrisisCtrl = function ($timeout, $scope, RisarApi) {
     }
     var init = function () {
         var hash = document.location.hash;
-        var prefix = "tab_";
-        hash = hash.replace(/\//, '')
         if (hash) {
-            $('.nav-pills a[href='+hash.replace(prefix,"")+']').tab('show') ;
+            hash.match('child') ? open_tab('#last') : open_tab(hash);
         }
-
-        // Change hash for page-reload
-        $('.nav-pills a').on('shown.bs.tab', function (e) {
-            window.location.hash = e.target.hash.replace("#", "#" + prefix);
-        })
         reload_epicrisis();
     };
     init();
