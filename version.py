c8af67186338128553b7a12d2684aec8ed130206
SAntipov <santipov@korusconsulting.ru>
2015-01-20 14:56:03 +0300

    TMIS-556: нумерация отчётов

--- version.py
+++ version.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
-version = u'1.1dev39'
-last_change_date = datetime(year=2015, month=1, day=19, hour=15,)
+version = u'1.1dev40'
+last_change_date = datetime(year=2015, month=1, day=20, hour=15,)
