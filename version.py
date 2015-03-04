b4dc44ab26fc8f5a6e4c3a7953fc057a026123c6
SAntipov <santipov@korusconsulting.ru>
2015-03-04 14:43:21 +0300

    v1.1dev57

--- version.py
+++ version.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
-version = u'1.1dev56'
-last_change_date = datetime(year=2015, month=2, day=27, hour=16)
+version = u'1.1dev57'
+last_change_date = datetime(year=2015, month=3, day=4, hour=15)
