67af38ccb3aa922c6c8bfeb041e7e177043d05d6
viruzzz-kun <viruzzz.soft@gmail.com>
2015-03-05 18:32:54 +0300

    RIMIS-186

--- blueprints/risar_config/static/js/app.js
+++ blueprints/risar_config/static/js/app.js
@@ -9,22 +9,35 @@ angular.module('WebMis20')
 .controller('RisarRoutingSetup', function ($scope, $http, RefBookService) {
     $scope.query = '';
     $scope.list = [];
+    $scope.saved = [];
     $scope.clipboard = null;
     $scope.MKB = RefBookService.get('MKB');
     $scope.copy_clipboard = function (row) {
         $scope.clipboard = row.diagnoses;
     };
     $scope.paste_clipboard = function (row) {
-        row.diagnoses = _.distinct(row.diagnoses.concat($scope.clipboard.clone()), 'id');
+        row.diagnoses = _.sortBy(_.distinct(row.diagnoses.concat($scope.clipboard.clone()), 'id'), 'code');
+    };
+    $scope.reset_row = function (index) {
+        $scope.list[index].diagnoses = $scope.saved[index].diagnoses.clone();
+    };
+    $scope.row_equal = function (index) {
+        return angular.equals($scope.list[index], $scope.saved[index]);
     };
     $scope.save = function () {
-        $http.post('/risar_config/api/routing.json', $scope.list).success(function (data) {
-            $scope.list = data.result;
-        })
+        $http.post('/risar_config/api/routing.json', $scope.list).success(init_data)
     };
-    $http.get('/risar_config/api/routing.json').success(function (data) {
-        $scope.list = data.result;
-    })
+    $http.get('/risar_config/api/routing.json').success(init_data);
+
+    var sortDiags = $scope.sortDiags = function (org) {
+        org.diagnoses = _.sortBy(org.diagnoses, 'code');
+        return org;
+    };
+
+    function init_data (data) {
+        $scope.list = data.result.map(sortDiags);
+        $scope.saved = _.deepCopy($scope.list);
+    }
 })
 .filter('orgdiag', function () {
     return function (orgs, query) {
