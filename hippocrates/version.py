838065d6457bd8b63eb02ce5a37596928f41dbbc
SAntipov <santipov@korusconsulting.ru>
2015-04-01 15:21:35 +0300

    TMIS-689: костылефикс получения current_user на клиенте (TODO: refactoring)

--- hippocrates/version.py
+++ hippocrates/version.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
-version = u'1.1.59'
-last_change_date = datetime(year=2015, month=3, day=30, hour=15)
+version = u'1.1.60'
+last_change_date = datetime(year=2015, month=4, day=1, hour=15)
