36c218c7172f3b04e64730a3aadd9408a017659f
plakrisenko <PLakrisenko@korusconsulting.ru>
2015-04-21 16:38:35 +0300

    RMIS-238 Переработка формы ввода данных повторного осмотра

--- hippocrates/blueprints/risar/static/js/checkup.js
+++ hippocrates/blueprints/risar/static/js/checkup.js
@@ -102,22 +102,25 @@ var CheckupSecondEditCtrl = function ($scope, $window, $document, RisarApi, Conf
             event_id: $scope.chart.id
         }
     };
-    $scope.save = function () {
-        if($scope.checkup){
-            $scope.checkup.flat_code = 'risarSecondInspection';
-        } else {
-            $scope.checkup = {flat_code: 'risarSecondInspection'};
-        }
-        var model = $scope.checkup;
-        RisarApi.checkup.save($scope.event_id, model)
-        .then(function (data) {
-            if($scope.checkup.id){
-                $scope.checkup = data;
+    $scope.save = function (form_controller) {
+        form_controller.submit_attempt = true;
+        if (form_controller.$valid){
+            if($scope.checkup){
+                $scope.checkup.flat_code = 'risarSecondInspection';
             } else {
-                $window.open(Config.url.inpection_edit_html + '?event_id=' + $scope.chart.id + '&checkup_id=' + data.id, '_self');
+                $scope.checkup = {flat_code: 'risarSecondInspection'};
             }
+            var model = $scope.checkup;
+            RisarApi.checkup.save($scope.event_id, model)
+            .then(function (data) {
+                if($scope.checkup.id){
+                    $scope.checkup = data;
+                } else {
+                    $window.open(Config.url.inpection_edit_html + '?event_id=' + $scope.chart.id + '&checkup_id=' + data.id, '_self');
+                }
 
-        })
+            })
+        }
     }
 };
 
