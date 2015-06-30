45062c86c0e18ee824a646dc3f13ab114a6e6fde
SAntipov <santipov@korusconsulting.ru>
2015-06-30 16:54:22 +0300

    v1.2dev3

--- hippocrates/version.py
+++ hippocrates/version.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
-version = u'1.2dev2'
-last_change_date = datetime(year=2015, month=6, day=5, hour=15)
+version = u'1.2dev3'
+last_change_date = datetime(year=2015, month=6, day=30, hour=17)
