03ab0bb9ce8332387ef307033be51522635947d2
swod <roman.s3000@gmail.com>
2015-08-12 12:48:05 +0300

    Version 1.2dev4

--- hippocrates/version.py
+++ hippocrates/version.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
-version = u'1.2dev3'
-last_change_date = datetime(year=2015, month=6, day=30, hour=17)
+version = u'1.2dev4'
+last_change_date = datetime(year=2015, month=8, day=12, hour=13)
