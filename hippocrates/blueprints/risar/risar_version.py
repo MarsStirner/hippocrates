7638a0b0f8734360b63fd66357ffe8eba9d0ea36
swod <roman.s3000@gmail.com>
2015-08-14 18:01:23 +0300

    risar version 2.2.1

--- hippocrates/blueprints/risar/risar_version.py
+++ hippocrates/blueprints/risar/risar_version.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
-version = u'2.2.0'
-last_change_date = datetime(year=2015, month=8, day=14, hour=17)
+version = u'2.2.1'
+last_change_date = datetime(year=2015, month=8, day=14, hour=18)
