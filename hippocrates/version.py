3d8946d84ea85ec765493103045e6c9a15a48dce
SAntipov <santipov@korusconsulting.ru>
2015-09-14 13:37:04 +0300

    TMIS-805: при создании обращения учитываем тип финансирования из предварительной записи

--- hippocrates/version.py
+++ hippocrates/version.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
-version = u'1.2'
-last_change_date = datetime(year=2015, month=8, day=25, hour=16)
+version = u'1.3dev1'
+last_change_date = datetime(year=2015, month=9, day=14, hour=13)
