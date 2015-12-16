2ed0dbe32d2b7581510b6e8f623b7f82dd804fe1
plakrisenko <PLakrisenko@korusconsulting.ru>
2015-12-16 19:15:09 +0300

    TMIS-941 интерфейс 'Биоматериалы', пробирки, фильтрация по статусам и биоматериалам

--- hippocrates/blueprints/biomaterials/static/js/index.js
+++ hippocrates/blueprints/biomaterials/static/js/index.js
@@ -1,16 +1,49 @@
-var IndexCtrl = function ($scope, $modal, $http, PrintingService) {
-    $scope.search_date = {execDate:new Date()};
+var IndexCtrl = function ($scope, $modal, $http, RefBookService, PrintingService) {
+    $scope.selected_records = [];
+    $scope.TTJStatus = RefBookService.get('TTJStatus');
+    $scope.filter = {execDate:new Date(),
+                     status: null
+    }
+
+    $scope.toggle_select_record = function (record) {
+        if ($scope.selected_records.has(record)) {
+            $scope.selected_records.remove(record);
+        } else {
+            $scope.selected_records.push(record);
+        }
+    };
+    $scope.select_all_records = function () {
+        if ($scope.selected_records.length == $scope.ttj_records.length) {
+            $scope.selected_records = [];
+        } else {
+            $scope.selected_records = $scope.ttj_records.map(function (record) {
+                if (!$scope.selected_records.has(record)) {
+                }
+                return record;
+            })
+        }
+    };
     $scope.get_data = function() {
-        $http.get(url_get_ttj_records, {
-            params: {
-                execDate: $scope.search_date.execDate
-            }
+        $http.post(url_get_ttj_records, {
+                filter: $scope.filter
         })
         .then(function (res) {
-                $scope.ttj_records = res.data.result;
+                $scope.ttj_records = res.data.result[0];
+                $scope.tubes = res.data.result[1];
+                $scope.number_by_status = res.data.result[2];
         });
     };
-    $scope.$watch('search_date.execDate', function (new_value, old_value) {
+    $scope.change_status = function(status){
+        var ids = $scope.selected_records.map(function(record){return record.id});
+        $http.post(url_ttj_update_status, {
+            ids: ids,
+            status: $scope.TTJStatus.get_by_code(status)
+        })
+        .success(function(result) {
+        })
+    }
+
+    $scope.$watchCollection('filter', function (new_value, old_value) {
         $scope.get_data();
     });
 };
--- hippocrates/blueprints/biomaterials/views/api_json.py
+++ hippocrates/blueprints/biomaterials/views/api_json.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import collections
+
 from flask import request
 from sqlalchemy import func
 
@@ -6,11 +8,47 @@ from ..app import module
 from nemesis.lib.apiutils import api_method
 from nemesis.lib.utils import safe_date
 from nemesis.models.actions import TakenTissueJournal
+from nemesis.models.enums import TTJStatus
+from nemesis.systemwide import db
 
 
-@module.route('/api/get_ttj_records.json', methods=['GET'])
+@module.route('/api/get_ttj_records.json', methods=['POST'])
 @api_method
 def api_get_ttj_records():
-    exec_date = safe_date(request.args.get('execDate'))
-    ttj_records = TakenTissueJournal.query.filter(func.date(TakenTissueJournal.datetimeTaken) == exec_date).all()
-    return ttj_records
+    data = request.json
+    number_by_status = {'all': 0, 'waiting': 0, 'in_progress': 0, 'sending_to_lab': 0, 'finished': 0}
+
+    filter = data.get('filter')
+
+    status = filter.get('status')
+    exec_date = safe_date(filter.get('execDate'))
+    biomaterial = filter.get('biomaterial')
+
+    test_tubes = collections.defaultdict(lambda: {'amount': 0})
+    query = TakenTissueJournal.query.filter(func.date(TakenTissueJournal.datetimeTaken) == exec_date)
+    if status is not None:
+        query = query.filter(TakenTissueJournal.statusCode == status)
+    if biomaterial:
+        query = query.filter(TakenTissueJournal.tissueType_id == biomaterial['id'])
+    ttj_records = query.all()
+    number_by_status['all'] = len(ttj_records)
+
+    def count_tubes(x, y):
+        x[y.testTubeType.code]['name'] = y.testTubeType.name
+        x[y.testTubeType.code]['amount'] += y.amount
+        number_by_status[TTJStatus.codes[y.status.value]] += 1
+        return x
+
+    reduce(count_tubes, ttj_records, test_tubes)
+    return ttj_records, test_tubes, number_by_status
+
+
+@module.route('/api/ttj_change_status.json', methods=['POST'])
+@api_method
+def api_ttj_change_status():
+    data = request.json
+    status = data.get('status')
+    ids = data.get('ids')
+    TakenTissueJournal.query.filter(TakenTissueJournal.id.in_(ids),).update({'status': status['id']},
+                                                                            synchronize_session=False)
+    db.session.commit()
