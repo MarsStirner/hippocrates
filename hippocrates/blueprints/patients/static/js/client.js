264cfb50d317cfae7949e5d7207c75fce6664fad
p.lakrisenko <PLakrisenko@korusconsulting.ru>
2016-01-14 17:57:00 +0300

    TMIS-936 импорт данных по талонам ВМП

--- hippocrates/blueprints/patients/static/js/client.js
+++ hippocrates/blueprints/patients/static/js/client.js
@@ -224,6 +224,14 @@ angular.module('WebMis20.controllers').
                 });
             };
 
+            $scope.addVmpCoupon= function(client){
+                $scope.clientServices.addVmpCoupon(client)
+            }
+
+            $scope.removeVmpCoupon= function(client){
+                $scope.clientServices.removeVmpCoupon(client)
+            }
+
             $scope.reloadClient();
         }
     ]);
