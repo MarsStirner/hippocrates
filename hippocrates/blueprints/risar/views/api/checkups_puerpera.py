8934463ab5ceb6a6c89d27f87294a386066e35a7
Dmitry Paschenko <paschenko@bars-open.ru>
2016-03-30 17:05:29 +0300

    RIMIS-822 Разработка функционала учёта осмотров специалистов ПЦ и родильниц

---
+++ hippocrates/blueprints/risar/views/api/checkups_puerpera.py
@@ -0,0 +1,99 @@
+# -*- encoding: utf-8 -*-
+from blueprints.risar.lib.fetus import create_or_update_fetuses
+from flask import request
+
+from blueprints.risar.app import module
+from blueprints.risar.lib.card import PregnancyCard
+from blueprints.risar.lib.pregnancy_dates import get_pregnancy_week
+from blueprints.risar.lib.represent import represent_checkup, represent_checkups, \
+    represent_fetuses
+from blueprints.risar.lib.utils import get_action_by_id, close_open_checkups
+from nemesis.lib.apiutils import api_method, ApiException
+from nemesis.lib.diagnosis import create_or_update_diagnoses
+from nemesis.lib.utils import safe_datetime
+from nemesis.models.event import Event
+from nemesis.systemwide import db
+
+
+@module.route('/api/0/checkup/', methods=['POST'])
+@module.route('/api/0/checkup/<int:event_id>', methods=['POST'])
+@api_method
+def api_0_checkup(event_id):
+    data = request.get_json()
+    checkup_id = data.pop('id', None)
+    flat_code = data.pop('flat_code', None)
+    beg_date = safe_datetime(data.pop('beg_date', None))
+    person = data.pop('person', None)
+    diagnoses = data.pop('diagnoses', None)
+    fetuses = data.pop('fetuses', None)
+
+    if not flat_code:
+        raise ApiException(400, 'flat_code required')
+
+    event = Event.query.get(event_id)
+    card = PregnancyCard.get_for_event(event)
+    action = get_action_by_id(checkup_id, event, flat_code, True)
+
+    if not checkup_id:
+        close_open_checkups(event_id)
+
+    action.begDate = beg_date
+
+    for code, value in data.iteritems():
+        if code in action.propsByCode:
+            action.propsByCode[code].value = value
+
+    create_or_update_diagnoses(action, diagnoses)
+
+    db.session.commit()
+    # после комита, чтобы получили ID нового действия перед созданием нового плода
+    create_or_update_fetuses(action, fetuses)
+    card.reevaluate_card_attrs()
+    db.session.commit()
+
+    # measure_mng = EventMeasureGenerator(action)
+    # measure_mng.generate_measures()
+    return represent_checkup(action)
+
+
+@module.route('/api/0/checkup/')
+@module.route('/api/0/checkup/<int:checkup_id>')
+@api_method
+def api_0_checkup_get(checkup_id=None):
+    action = get_action_by_id(checkup_id)
+    if not action:
+        raise ApiException(404, 'Action with id {0} not found'.format(checkup_id))
+    return represent_checkup(action)
+
+
+@module.route('/api/0/checkup/new/', methods=['POST'])
+@module.route('/api/0/checkup/new/<int:event_id>', methods=['POST'])
+@api_method
+def api_0_checkup_new(event_id):
+    data = request.get_json()
+    flat_code = data.get('flat_code')
+    if not flat_code:
+        raise ApiException(400, 'flat_code required')
+    event = Event.query.get(event_id)
+    action = get_action_by_id(None, event, flat_code, True)
+    result = represent_checkup(action)
+    result['pregnancy_week'] = get_pregnancy_week(event)
+    return result
+
+
+@module.route('/api/0/checkup_list/')
+@module.route('/api/0/checkup_list/<int:event_id>')
+@api_method
+def api_0_checkup_list(event_id):
+    event = Event.query.get(event_id)
+    return {
+        'checkups': represent_checkups(event)
+    }
+
+
+@module.route('/api/0/fetus_list/')
+@module.route('/api/0/fetus_list/<int:event_id>')
+@api_method
+def api_0_fetus_list(event_id):
+    event = Event.query.get(event_id)
+    return represent_fetuses(event)
