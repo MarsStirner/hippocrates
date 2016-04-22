556f18b12b33681ba621ee7e9d5ac58a563f5c04
Роман Степанов <r.stepanov@bars-open.ru>
2016-04-22 14:30:11 +0300

    Исправлена установка версии приложения для логгера

--- hippocrates/usagicompat.py
+++ hippocrates/usagicompat.py
@@ -3,12 +3,14 @@ import os
 
 from nemesis.app import app, bootstrap_app
 from tsukino_usagi.client import TsukinoUsagiClient
+from version import version as app_version
 
 __author__ = 'viruzzz-kun'
 
 
 class HippoUsagiClient(TsukinoUsagiClient):
     def on_configuration(self, configuration):
+        configuration['APP_VERSION'] = app_version
         app.config.update(configuration)
         bootstrap_app(os.path.join(os.path.dirname(__file__), 'templates'))
 
