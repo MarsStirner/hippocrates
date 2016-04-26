e4ed17286a5677647f4a795aa780c9fa6cb6115a
Роман Степанов <r.stepanov@bars-open.ru>
2016-04-26 16:49:16 +0300

    Version 1.3.6

--- hippocrates/version.py
+++ hippocrates/version.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
-version = u'1.3.5'
-last_change_date = datetime(year=2016, month=4, day=15, hour=14)
+version = u'1.3.6'
+last_change_date = datetime(year=2016, month=4, day=26, hour=17)
