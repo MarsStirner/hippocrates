ff95102760af14ec47df9efde3f912a52f68b386
Михаил Мальков <m.malkov@bars-open.ru>
2016-05-17 11:28:37 +0300

    TMIS-1098 Осмысленные сообщения об ошибках при создании действия. Общий механизм сообщения об ошибках.

--- hippocrates/blueprints/actions/views/html.py
+++ hippocrates/blueprints/actions/views/html.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from flask import request, render_template, abort
+from flask import request, render_template
+from nemesis.lib.html_utils import UIException
 from ..app import module
-from nemesis.lib.utils import safe_int
+from nemesis.lib.utils import safe_int, bail_out
 from nemesis.lib.action.utils import check_action_service_requirements
 
 
@@ -12,12 +13,17 @@ __author__ = 'viruzzz-kun'
 @module.route('/action.html')
 def html_action():
     if 'action_id' not in request.args:
-        action_type_id = safe_int(request.args.get('action_type_id'))
-        if not action_type_id:
-            raise abort(400, '`action_type_id` required')
+        action_type_id = safe_int(request.args.get('action_type_id')) \
+                         or bail_out(UIException(400, u'Необходимо указать id типа действия`action_type_id`'))
+        safe_int(request.args.get('event_id')) \
+            or bail_out(UIException(400, u'Необходимо указать id обращения `event_id`'))
         price_list_item_id = safe_int(request.args.get('price_list_item_id'))
         service_check = check_action_service_requirements(action_type_id, price_list_item_id)
         if not service_check['result']:
-            raise abort(400, u'Ошбика настроек услуг и прайс-листов')  # service_check['message']
+            raise UIException(
+                400,
+                u'Ошибка настроек услуг и прайс-листов: %s' % service_check['message'],
+                u'Ошибка настроек услуг и прайс-листов'
+            )
 
     return render_template('actions/action.html')
