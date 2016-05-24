27388aee7c273bf3802e309aa125339866cdbc09
Dmitry Paschenko <paschenko@bars-open.ru>
2016-05-24 14:50:44 +0300

    RIMIS-837 Не отображаются пациентки в расширенном поиске при высталении фильтра "Дата родов"

--- hippocrates/blueprints/risar/views/api/search.py
+++ hippocrates/blueprints/risar/views/api/search.py
@@ -2,6 +2,7 @@
 import datetime
 import json
 import math
+import time
 
 from blueprints.reports.jasper_client import JasperReport
 from flask import request, make_response
@@ -34,6 +35,12 @@ def sphinx_days(date_string):
     return int(timegm(date.timetuple()) / 86400)
 
 
+def sphinx_local_days(date_string):
+    date_string = date_string[:10]
+    date = datetime.datetime.strptime(date_string, '%Y-%m-%d')
+    return int(time.mktime(date.timetuple()))
+
+
 def search_events(paginated=True, **kwargs):
     from nemesis.lib.sphinx_search import Search, SearchConfig
 
@@ -56,15 +63,15 @@ def search_events(paginated=True, **kwargs):
             risk = kwargs['risk']
         query = query.filter(risk__in=risk)
     if 'bdate_from' in kwargs:
-        query = query.filter(bdate__gte=sphinx_days(kwargs['bdate_from']))
+        query = query.filter(bdate__gte=sphinx_local_days(kwargs['bdate_from']))
     if 'bdate_to' in kwargs:
-        query = query.filter(bdate__lte=sphinx_days(kwargs['bdate_to']))
+        query = query.filter(bdate__lte=sphinx_local_days(kwargs['bdate_to']))
     if 'psdate' in kwargs:
-        query = query.filter(psdate__eq=sphinx_days(kwargs['psdate']))
+        query = query.filter(psdate__eq=sphinx_local_days(kwargs['psdate']))
     if 'checkup_date_from' in kwargs:
-        query = query.filter(checkups__gte=sphinx_days(kwargs['checkup_date_from']))
+        query = query.filter(checkups__gte=sphinx_local_days(kwargs['checkup_date_from']))
     if 'checkup_date_to' in kwargs:
-        query = query.filter(checkups__lte=sphinx_days(kwargs['checkup_date_to']))
+        query = query.filter(checkups__lte=sphinx_local_days(kwargs['checkup_date_to']))
     if 'closed' in kwargs:
         if kwargs['closed']:
             query = query.filter(exec_date__neq=0)
