32721d1f0a7b3ac1b995b05df2df08256cb99ce4
Aleksandr Krivopustov <a.krivopustov@bars-open.ru>
2016-05-31 21:04:18 +0300

    RISAR Version 2.3.11

--- hippocrates/blueprints/risar/risar_version.py
+++ hippocrates/blueprints/risar/risar_version.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
-version = u'2.3.10'
-last_change_date = datetime(year=2016, month=5, day=31, hour=12)
+version = u'2.3.11'
+last_change_date = datetime(year=2016, month=5, day=31, hour=21)
