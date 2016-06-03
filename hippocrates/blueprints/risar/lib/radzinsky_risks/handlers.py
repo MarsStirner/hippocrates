7c8a39cf760719d0c596c21f4d46a5c4baf5269b
Роман Степанов <r.stepanov@bars-open.ru>
2016-06-03 20:45:14 +0300

    RIMIS-682 - расчеты

--- hippocrates/blueprints/risar/lib/radzinsky_risks/handlers.py
+++ hippocrates/blueprints/risar/lib/radzinsky_risks/handlers.py
@@ -2,7 +2,14 @@
 
 import itertools
 
-from nemesis.lib.utils import safe_bool, safe_bool_none
+from .utils import (
+    _iter_preg_child, _mkb_match,
+    _filter_child_abnormal_weight, _filter_child_cong_prev_preg, _filter_child_death,
+    _filter_child_miscarriage, _filter_child_neuro_prev_preg, _filter_misbirth_prev_preg,
+    _filter_parity_prev_preg, _filter_premature_prev_preg, _filter_prev_preg_compl,
+    _filter_prev_preg_tubal
+)
+from nemesis.lib.utils import safe_bool
 
 
 def mother_younger_18(card):
@@ -87,17 +94,6 @@ def not_married(card):
     return False
 
 
-def _filter_parity_prev_preg(prev_preg):
-    """
-    На форме «Анамнез пациентки» на вкладке «Сведения о преыдущих беременностях»,
-    у которых атрибут «Исход беременности» = преждевременные роды 22-27 недель
-    или преждевременные роды 28-37 недель или запоздалые роды или роды в срок.
-    """
-    return prev_preg.action['pregnancyResult'].value_raw in (
-        'premature_birth_22-27', 'premature_birth_28-37', 'postmature_birth', 'delivery'
-    )
-
-
 def parity_under_7(card):
     prev_pr_count = len(filter(_filter_parity_prev_preg, card.prev_pregs))
     return 4 <= prev_pr_count <= 7
@@ -107,18 +103,6 @@ def parity_above_8(card):
     prev_pr_count = len(filter(_filter_parity_prev_preg, card.prev_pregs))
     return 8 <= prev_pr_count
 
-
-def _filter_misbirth_prev_preg(prev_preg):
-    """
-    На форме «Анамнез пациентки» на вкладке «Сведения о преыдущих беременностях» записи
-    со значением атрибута «Исход беременности» = самопроизвольный выкидыш до 11 недель или
-    самопроизвольный выкидыш 12-21 недель или медицинский аборт до 12 недель или
-    аборт по мед.показаниям или неуточненный выкидыш
-    """
-    return prev_preg.action['pregnancyResult'].value_raw in (
-        'misbirth_before_11', 'misbirth_before_12-21', 'therapeutic_abortion_before_12',
-        'therapeutic_abortion', 'unknown_miscarriage'
-    )
 
 def abortion_1(card):
     parity_count_gt_zero = any(itertools.ifilter(_filter_parity_prev_preg, card.prev_pregs))
@@ -151,41 +135,14 @@ def intrauterine_operations(card):
     return False
 
 
-def _filter_premature_prev_preg(prev_preg):
-    """
-    На форме «Анамез пациентки» на вкладке «Сведения о предыдущих беременностях» записи
-    со значением атрибута «Исход беременности» = преждевременные роды 22-27 недель или
-    преждевременные роды 28-37 недель
-    """
-    return prev_preg.action['pregnancyResult'].value_raw in (
-        'premature_birth_22-27', 'premature_birth_28-37'
-    )
-
-
 def premature_birth_1(card):
-    premature_count = len(filter(_filter_parity_prev_preg, card.prev_pregs))
+    premature_count = len(filter(_filter_premature_prev_preg, card.prev_pregs))
     return premature_count == 1
 
 
 def premature_birth_more_2(card):
-    premature_count = len(filter(_filter_parity_prev_preg, card.prev_pregs))
+    premature_count = len(filter(_filter_premature_prev_preg, card.prev_pregs))
     return premature_count >= 2
-
-
-def _iter_preg_child(prev_pregs):
-    for prev_preg in prev_pregs:
-        for child in prev_preg.newborn_inspections:
-            yield child
-
-
-def _filter_child_miscarriage(child):
-    """
-    На форме «Анамез пациентки» на вкладке «Сведения о предыдущих беременностях» записи
-    со значением в сведении о ребенке атрибута «Живой» = нет и в поле Умер в срок указано
-    значение «интранатально» или «антенатально»
-    """
-    alive = safe_bool_none(child.alive)
-    return alive is False and child.died_at_code in ('01', '05')
 
 
 def miscarriage_1(card):
@@ -202,16 +159,6 @@ def miscarriage_more_2(card):
     return miscarriage_count >= 2
 
 
-def _filter_child_death(child):
-    """
-    На форме «Анамез пациентки» на вкладке «Сведения о предыдущих беременностях» записи
-    со значением в сведении о ребенке атрибута «Живой» = нет и в поле Умер в срок указано
-    «7-27 дней»
-    """
-    alive = safe_bool_none(child.alive)
-    return alive is False and child.died_at_code in ('03',)
-
-
 def child_death_1(card):
     ch_d_count = len(list(itertools.ifilter(
         _filter_child_death, _iter_preg_child(card.prev_pregs)
@@ -226,29 +173,11 @@ def child_death_more_2(card):
     return ch_d_count >= 2
 
 
-def _filter_child_cong_prev_preg(child):
-    """
-    На форме «Анамез пациентки» на вкладке «Сведения о предыдущих беременностях» записи
-    с установленным флажком в сведении о ребенке атрибута Аномалии развития
-    """
-    abnormal_development = safe_bool_none(child.abnormal_development)
-    return abnormal_development is True
-
-
 def congenital_disorders(card):
     cong_gt_zero = any(itertools.ifilter(
         _filter_child_cong_prev_preg, _iter_preg_child(card.prev_pregs)
     ))
     return cong_gt_zero
-
-
-def _filter_child_neuro_prev_preg(child):
-    """
-    На форме «Анамез пациентки» на вкладке «Сведения о предыдущих беременностях» записи
-    с установленным флажком в сведении о ребенке атрибута Неврологические нарушения
-    """
-    neurological_disorders = safe_bool_none(child.neurological_disorders)
-    return neurological_disorders is True
 
 
 def neurological_disorders(card):
@@ -258,19 +187,13 @@ def neurological_disorders(card):
     return neuro_gt_zero
 
 
-def _filter_child_abnormal_weight(child):
-    """
-    На форме «Анамез пациентки» на вкладке «Сведения о предыдущих беременностях» записи
-    в сведении о ребенке со значением атрибута Масса,г  <2500 г или >4000 г
-    """
-    if child.weight is not None:
-        return child.weight < 2500 or child.weight > 4000
-    return False
-
-
 def abnormal_child_weight(card):
     abn_weight_gt_zero = any(itertools.ifilter(
-        _filter_child_abnormal_weight, _iter_preg_child(card.prev_pregs)
+        _filter_child_abnormal_weight, _iter_preg_child(
+            prev_preg
+            for prev_preg in card.prev_pregs
+            if prev_preg.action['pregnancyResult'].value_raw == 'delivery'
+        )
     ))
     return abn_weight_gt_zero
 
@@ -292,19 +215,19 @@ def infertility_more_5(card):
 
 
 def uterine_scar(card):
-    return 'O34.2' in card.unclosed_mkbs
+    return _mkb_match(card.unclosed_mkbs, 'O34.2')
 
 
 def uterus_oophoron_tumor(card):
-    return 'O34.1' in card.unclosed_mkbs
+    return _mkb_match(card.unclosed_mkbs, 'O34.1')
 
 
 def insuficiencia_istmicocervical(card):
-    return 'O34.3' in card.unclosed_mkbs
+    return _mkb_match(card.unclosed_mkbs, 'O34.3')
 
 
 def uterine_malformations(card):
-    return any(mkb in card.unclosed_mkbs for mkb in ('O34.0', 'O34.4'))
+    return _mkb_match(card.unclosed_mkbs, ('O34.0', 'O34.4'))
 
 
 def chronic_inflammation(card):
@@ -315,17 +238,22 @@ def chronic_inflammation(card):
     есть хотя бы одна запись со значением атрибута "Осложнения после родов/абортов"
     из узла О85-О92.
     """
-    # TODO
-    return False
+    if _mkb_match(card.unclosed_mkbs, needles=u'N70-N77.99'):
+        return True
+
+    anamnesis = card.anamnesis.mother
+    if anamnesis and '2' in anamnesis['contraception'].value_raw:
+        return True
+
+    return any(itertools.ifilter(
+        _filter_prev_preg_compl, card.prev_pregs
+    ))
 
 
 def tubal_pregnancy(card):
-    """
-    На форме «Анамез пациентки» на вкладке «Сведения о предыдущих беременностях»
-    есть хотя бы одна запись со значением атрибута
-    «Патологии беременности» = какому-либо коду МКБ из узла O00.
-    """
-    return False
+    return any(itertools.ifilter(
+        _filter_prev_preg_tubal, card.prev_pregs
+    ))
 
 
 def extracorporal_fertilization(card):
@@ -339,4 +267,118 @@ def intracytoplasmic_sperm_injection(card):
     anamnesis = card.anamnesis.mother
     if anamnesis:
         return anamnesis['fertilization_type'].value_raw == '03'
+    return False
+
+
+def heart_disease(card):
+    return _mkb_match(card.unclosed_mkbs, needles=u'Q20.0-Q26.9, I05.0-I09.9, I34.0-I38, I42.0, O99.4')
+
+
+def heart_disease_circulatory_embarrassment(card):
+    """
+    На форме "Анамнез пациентки" поставлен флажок "Пороки сердца с нарушением кровообращения"
+    или на форме "Диагнозы случая" есть хотя бы один незакрытый диагноз из узлов I26-I28.
+    """
+    # TODO: after anamnesis ticket
+    # anamnesis = card.anamnesis.mother
+    # if anamnesis:
+    #     return safe_bool(anamnesis[<code>].value)
+    return _mkb_match(card.unclosed_mkbs, needles=u'I26-I28.99')
+
+
+def hypertensive_disease_1(card):
+    return _mkb_match(card.unclosed_mkbs, needles=u'I11-I11.99')
+
+
+def hypertensive_disease_2(card):
+    return _mkb_match(card.unclosed_mkbs, needles=u'I12-I12.99')
+
+
+def hypertensive_disease_3(card):
+    return _mkb_match(card.unclosed_mkbs, needles=u'I13-I13.99')
+
+
+def varicose(card):
+    return _mkb_match(card.unclosed_mkbs, needles=u'I83-I83.99, O22-O22.99')
+
+
+def hypotensive_syndrome(card):
+    return _mkb_match(card.unclosed_mkbs, needles=u'O26.5, I95-I95.99')
+
+
+def renal_disease(card):
+    return _mkb_match(card.unclosed_mkbs, needles=u'N00-N39.99')
+
+
+def adrenal_disorders(card):
+    return _mkb_match(card.unclosed_mkbs,
+                      needles=u'C74.0, C74.1, C74.9, D35.0, E27.0-E27.9, Q89.1, E34.5, E25.0')
+
+
+def neurometabolic_endocrine_syndrome(card):
+    return _mkb_match(card.unclosed_mkbs,
+                      needles=u'N91-N91.99, N92.0-N92.3, N92.5, N92.6, N93-N93.99, Q87.4, E28-E28.99')
+
+
+def diabetes(card):
+    return _mkb_match(card.unclosed_mkbs, needles=u'E10.0-E14.9, O24.0-O24.4, О24.9')
+
+
+def thyroid_disorders(card):
+    return _mkb_match(card.unclosed_mkbs, needles=u'C73, E00.0-E07.9, A18.8, O99.2')
+
+
+def obesity(card):
+    """
+    На форме «Первичный осмотр» значение атрибута «Индекс массы тела» >=30
+    или на форме "Диагнозы случая" присутствует хоть один диагноз из группы E65-E68
+    """
+    # TODO: first_inspection
+    return _mkb_match(card.unclosed_mkbs, needles=u'E65.99-E68.99')
+
+
+def anemia_90(card):
+    # TODO: measures
+    return False
+
+
+def anemia_100(card):
+    # TODO: measures
+    return False
+
+
+def anemia_110(card):
+    # TODO: measures
+    return False
+
+
+def coagulopathy(card):
+    return _mkb_match(card.unclosed_mkbs, needles=u'D65-D89.9, O46.0, O99.1')
+
+
+def myopia(card):
+    return _mkb_match(card.unclosed_mkbs,
+                      needles=u'H44.2, H52.1, H40.0-H40.9, H43.1, H44.5, H46-H47.7, H33.0-H35.9, O99.8')
+
+
+def persistent_infection(card):
+    return _mkb_match(
+        card.unclosed_mkbs,
+        needles=(u'B20.0-B24, R75-R75.99, A15-A19.9, A23.0-A23.9, B58-B58.99, '
+                 u'B18.0-B19.9, Z21-Z21.99, Z22.5, O98.0-O98.9')
+    )
+
+
+def lupus_anticoagulant_positive(card):
+    # TODO: measures
+    return False
+
+
+def antiphospholipid_antibodies_IgG(card):
+    # TODO: measures
+    return False
+
+
+def antiphospholipid_antibodies_IgM(card):
+    # TODO: measures
     return False
