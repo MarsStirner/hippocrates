60be2d880125aa9b230b1852352d55da77e34f3e
Роман Степанов <r.stepanov@bars-open.ru>
2016-08-05 19:38:53 +0300

    RISAR Version 2.4.0

--- hippocrates/blueprints/risar/risar_version.py
+++ hippocrates/blueprints/risar/risar_version.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
-version = u'2.3.7'
-last_change_date = datetime(year=2016, month=5, day=12, hour=13)
+version = u'2.4.0'
+last_change_date = datetime(year=2016, month=8, day=5, hour=19)
