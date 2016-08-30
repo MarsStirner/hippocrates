31396ccea85644888738a9ea6d18be234c648bd8
Роман Степанов <r.stepanov@bars-open.ru>
2016-08-30 16:26:54 +0300

    RIMIS-1170 - методы интеграции получения данных талона посещения для 4 видов осмотров - доработки по оставшимся полям

--- hippocrates/blueprints/risar/views/api/integration/checkup_ticket25_xform.py
+++ hippocrates/blueprints/risar/views/api/integration/checkup_ticket25_xform.py
@@ -3,9 +3,8 @@ import logging
 
 from hippocrates.blueprints.risar.views.api.integration.xform import CheckupsXForm, wrap_simplify
 from hippocrates.blueprints.risar.views.api.integration.schemas import Schema
+from hippocrates.blueprints.risar.lib.represent.common import represent_action_diagnoses
 
-from nemesis.lib.apiutils import ApiException
-from nemesis.systemwide import db
 from nemesis.lib.utils import safe_date, safe_traverse
 from nemesis.models.person import Person
 
@@ -116,25 +115,29 @@ class CheckupsTicket25XFormSchema(Schema):
                     "description": "Травма, справочник rbTraumaType",
                     "type": "string"
                 },
-                "operation": {
-                    "description": "Операция",
-                    "type": "object",
-                    "properties": {
-                        "operation_code": {
-                            "description": "Операция (код)",
-                            "type": "string"
-                        },
-                        "operation_anesthesia": {
-                            "description": "Анестезия, справочник rbRisarOperationAnesthetization",
-                            "type": "string"
-                        },
-                        "operation_equipment": {
-                            "description": "Операция проведена с использованием аппаратуры, справочник rbRisarOperationEquipment",
-                            "type": "string"
-                        },
-                        "operation_doctor": {
-                            "description": "Врач операции (код врача)",
-                            "type": "string"
+                "operations": {
+                    "description": "Операции",
+                    "type": "array",
+                    "items": {
+                        "description": "Операция",
+                        "type": "object",
+                        "properties": {
+                            "operation_code": {
+                                "description": "Операция (код)",
+                                "type": "string"
+                            },
+                            "operation_anesthesia": {
+                                "description": "Анестезия, справочник rbRisarOperationAnesthetization",
+                                "type": "string"
+                            },
+                            "operation_equipment": {
+                                "description": "Операция проведена с использованием аппаратуры, справочник rbRisarOperationEquipment",
+                                "type": "string"
+                            },
+                            "operation_doctor": {
+                                "description": "Врач операции (код врача)",
+                                "type": "string"
+                            }
                         }
                     }
                 },
@@ -155,27 +158,31 @@ class CheckupsTicket25XFormSchema(Schema):
                         }
                     }
                 },
-                "sick_leave": {
-                    "description": "Документ о временной нетрудоспособности",
-                    "type": "object",
-                    "properties": {
-                        "sick_leave_type": {
-                            "description": "Документ о временной нетрудоспособности, справочник rbRisarSickLeaveType",
-                            "type": "string"
-                        },
-                        "sick_leave_reason": {
-                            "description": "Повод выдачи, справочник rbRisarSickLeaveReason",
-                            "type": "string"
-                        },
-                        "sick_leave_date_open": {
-                            "description": "Дата выдачи",
-                            "type": "string",
-                            "format": "date"
-                        },
-                        "sick_leave_date_close": {
-                            "description": "Дата закрытия",
-                            "type": "string",
-                            "format": "date"
+                "sick_leaves": {
+                    "description": "Документы о временной нетрудоспособности",
+                    "type": "array",
+                    "items": {
+                        "description": "Документ о временной нетрудоспособности",
+                        "type": "object",
+                        "properties": {
+                            "sick_leave_type": {
+                                "description": "Документ о временной нетрудоспособности, справочник rbRisarSickLeaveType",
+                                "type": "string"
+                            },
+                            "sick_leave_reason": {
+                                "description": "Повод выдачи, справочник rbRisarSickLeaveReason",
+                                "type": "string"
+                            },
+                            "sick_leave_date_open": {
+                                "description": "Дата выдачи",
+                                "type": "string",
+                                "format": "date"
+                            },
+                            "sick_leave_date_close": {
+                                "description": "Дата закрытия",
+                                "type": "string",
+                                "format": "date"
+                            }
                         }
                     }
                 },
@@ -205,6 +212,7 @@ class CheckupsTicket25XForm(CheckupsXForm):
         if not self.ticket25:
             return
 
+        inspection = self.target_obj
         action = self.ticket25
         person = action.person
         res = {
@@ -217,27 +225,34 @@ class CheckupsTicket25XForm(CheckupsXForm):
             'initial_treatment': self.or_undefined(self.from_rb(action['initial_treatment'].value)),
             'treatment_result': self.or_undefined(self.from_rb(action['treatment_result'].value)),
             'payment': self.or_undefined(self.from_rb(action['payment'].value)),
-            # 'visit_dates': '?',
-            # 'preliminary_diagnosis': '?',
-            # 'preliminary_reason': '?',
+            'visit_dates': self.or_undefined(safe_date(inspection.begDate)),
             'medical_services': self.or_undefined(self._repr_med_services(action)),
-            # 'diagnosis': '?',
-            # 'reason': '?',
-            # 'diagnosis_sop': '?',
-            # 'disease_character': '?',
-            # 'dispensary': '?',
-            # 'trauma': '?',
-            # 'operation': '?',
+            'operations': self.or_undefined(self._repr_operations(action)),
             'manipulations': self.or_undefined(self._repr_manipulations(action)),
-            # 'sick_leave': '?'
+            'sick_leaves': self.or_undefined(self._repr_sick_leaves(action)),
         }
-
+        res.update(self._repr_diagnoses_info(inspection))
         return res
 
     def _repr_med_services(self, action):
         return [
             safe_traverse(service, 'service', 'code')
             for service in action['services'].value
+        ]
+
+    def _repr_operations(self, action):
+        def make_operation_data(oper):
+            doctor_id = safe_traverse(oper, 'person', 'id')
+            doctor = Person.query.get(doctor_id) if doctor_id else None
+            return {
+                'operation_code': safe_traverse(oper, 'service', 'code'),
+                'operation_anesthesia': safe_traverse(oper, 'anesthetization', 'code'),
+                'operation_equipment': safe_traverse(oper, 'equipment', 'code'),
+                'operation_doctor': self.from_person_rb(doctor)
+            }
+        return [
+            make_operation_data(oper)
+            for oper in action['operations'].value
         ]
 
     def _repr_manipulations(self, action):
@@ -252,3 +267,45 @@ class CheckupsTicket25XForm(CheckupsXForm):
             make_manipulation_data(manip)
             for manip in action['manipulations'].value
         ]
+
+    def _repr_sick_leaves(self, action):
+        def make_td_data(td):
+            return {
+                'sick_leave_type': safe_traverse(td, 'type', 'code'),
+                'sick_leave_reason': safe_traverse(td, 'reason', 'code'),
+                'sick_leave_date_open': safe_date(safe_traverse(td, 'beg_date')),
+                'sick_leave_date_close': safe_date(safe_traverse(td, 'end_date')),
+            }
+        return [
+            make_td_data(td)
+            for td in action['temp_disability'].value
+        ]
+
+    @wrap_simplify
+    def _repr_diagnoses_info(self, inspection):
+        main_diag = None
+        compl_assoc_mkbs = []
+
+        diag_list = represent_action_diagnoses(inspection)
+        for diag in diag_list:
+            if main_diag is None:
+                for diagnosis_kind in diag['diagnosis_types'].values():
+                    if diagnosis_kind.code == 'main':
+                        main_diag = diag
+                        break
+                else:
+                    compl_assoc_mkbs.append(diag['diagnostic']['mkb'].DiagID)
+            else:
+                compl_assoc_mkbs.append(diag['diagnostic']['mkb'].DiagID)
+
+        res = {
+            'diagnosis_sop': self.or_undefined(compl_assoc_mkbs)
+        }
+        if main_diag is not None:
+            res.update({
+                'diagnosis': self.or_undefined(self.from_mkb_rb(main_diag['diagnostic']['mkb'])),
+                'reason': self.or_undefined(self.from_mkb_rb(main_diag['diagnostic']['mkb2'])),
+                'disease_character': self.or_undefined(self.from_rb(main_diag['diagnostic']['character'])),
+                'trauma': self.or_undefined(self.from_rb(main_diag['diagnostic']['trauma'])),
+            })
+        return res
