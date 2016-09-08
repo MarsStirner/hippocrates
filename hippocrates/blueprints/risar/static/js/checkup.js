91725228bbb94c3271c126218ca4031a16379240
Роман Степанов <r.stepanov@bars-open.ru>
2016-09-08 19:55:20 +0300

    RIMIS-1460 - в осмотрах на шаге талона посещения при переключении на шаг дата начала, если еще не указана, устанавливается равной дате начала осмотра

--- hippocrates/blueprints/risar/static/js/checkup.js
+++ hippocrates/blueprints/risar/static/js/checkup.js
@@ -73,3 +73,23 @@ function ($scope, $timeout, RisarApi, RefBookService, PrintingService, PrintingD
     };
 }])
 ;
+
+
+WebMis20.controller('CheckupTicket25Ctrl', ['$scope', function ($scope) {
+    var setTicket25Data = function () {
+        if ($scope.checkup && $scope.checkup.ticket_25 && !$scope.checkup.ticket_25.beg_date) {
+            $scope.checkup.ticket_25.beg_date = new Date($scope.checkup.beg_date);
+        }
+    };
+
+    $scope.$watch(function () {
+        return $scope.rc.sampleWizard.currentIndex;
+    }, function (newIndex) {
+        if (newIndex === $scope.thisRcStep - 1) setTicket25Data();
+    });
+
+    $scope.init = function (rc_step) {
+        $scope.thisRcStep = rc_step;
+    };
+}])
+;
