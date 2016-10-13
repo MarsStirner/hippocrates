c3bc431948ae8a59ae2c618b484916f807453d20
Aleksandr Krivopustov <a.krivopustov@bars-open.ru>
2016-10-13 19:47:31 +0300

    rimis1465 '0' для  MaternalCertificate deleted, поправлен represent

--- hippocrates/blueprints/risar/lib/represent/mat_cert.py
+++ hippocrates/blueprints/risar/lib/represent/mat_cert.py
@@ -4,13 +4,15 @@ __author__ = 'viruzzz-kun'
 
 
 def represent_mat_cert(mat_cert):
+    if not mat_cert:
+        return None,
     return {
-        'id': mat_cert.id if mat_cert else None,
-        'date': mat_cert.date if mat_cert else None,
-        'series': mat_cert.series if mat_cert else None,
-        'number': mat_cert.number if mat_cert else None,
-        'issuing_LPU_free_input': mat_cert.issuing_LPU_free_input if mat_cert else None,
-        'issuing_LPU_id': mat_cert.issuing_LPU_id if mat_cert else None,
-        'lpu': mat_cert.lpu if mat_cert else None,
-        'event_id': mat_cert.event_id if mat_cert else None,
+        'id': mat_cert.id,
+        'date': mat_cert.date,
+        'series': mat_cert.series,
+        'number': mat_cert.number,
+        'issuing_LPU_free_input': mat_cert.issuing_LPU_free_input,
+        'issuing_LPU_id': mat_cert.issuing_LPU_id,
+        'lpu': mat_cert.lpu,
+        'event_id': mat_cert.event_id,
     }
