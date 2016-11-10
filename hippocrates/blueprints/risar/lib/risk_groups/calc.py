d13d88270d33bcafd29f8fcc15e52a42ff084e60
Aleksandr Krivopustov <a.krivopustov@bars-open.ru>
2016-11-10 18:54:32 +0300

    RIMIS_1656 поля на форму талона в осмотре, просмотр талона  во всех осмотрах, bmi

--- hippocrates/blueprints/risar/lib/risk_groups/calc.py
+++ hippocrates/blueprints/risar/lib/risk_groups/calc.py
@@ -448,7 +448,8 @@ def calc_risk_groups(card):
 
     p2 = not card.prev_pregs
     p3 = card.event.client.age_tuple()[-1] > 35
-    p4 = (card.checkups and checkups[0]['BMI'].value >= 25)
+    bmi = checkups[0].get('BMI')
+    p4 = (card.checkups and checkups[0]['BMI'].value >= 25) if bmi else False
     p5 = card.anamnesis.mother['preeclampsia'].value
 
     p6 = any(
