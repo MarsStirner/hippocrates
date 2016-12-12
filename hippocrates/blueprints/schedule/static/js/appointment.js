ee7e2d52d066b7fa031e79c4f17b11f39c6f9743
Oleg Tkachenko <o.tkachenko@bars-open.ru>
2016-12-12 15:41:08 +0300

    RIMIS-1796 Отображать расписание текущего пользователя при переходе в запись на прием

--- hippocrates/blueprints/schedule/static/js/appointment.js
+++ hippocrates/blueprints/schedule/static/js/appointment.js
@@ -1,14 +1,15 @@
 /**
  * Created by mmalkov on 11.07.14.
  */
-var PersonAppointmentCtrl = function ($scope, $http, RefBook, WMAppointmentDialog, PersonTreeUpdater) {
+var PersonAppointmentCtrl = function ($scope, $http, RefBook, WMAppointmentDialog, PersonTreeUpdater, CurrentUser) {
     $scope.modal = {};
     $scope.max_tickets = [];
     $scope.person_schedules = [];
     $scope.user_schedules = [];
     $scope.total_schedules = [];
 
-    $scope.user_selected = [];
+    $scope.current_user = CurrentUser.info;
+    $scope.user_selected = [$scope.current_user.id];
     $scope.data_selected = [];
     $scope.foldedArray = [];
 
@@ -194,4 +195,4 @@ var PersonAppointmentCtrl = function ($scope, $http, RefBook, WMAppointmentDialo
 
     $scope.monthChanged();
 };
-WebMis20.controller('PersonAppointmentCtrl', ['$scope', '$http' , 'RefBook', 'WMAppointmentDialog', 'PersonTreeUpdater', PersonAppointmentCtrl]);
+WebMis20.controller('PersonAppointmentCtrl', ['$scope', '$http' , 'RefBook', 'WMAppointmentDialog', 'PersonTreeUpdater', 'CurrentUser', PersonAppointmentCtrl]);
