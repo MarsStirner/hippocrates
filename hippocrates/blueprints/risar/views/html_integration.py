42551e1552224d96c824173009c38c12c389d914
Dmitry Paschenko <paschenko@bars-open.ru>
2017-01-20 22:04:12 +0300

    RIMIS-1965 Тамбов. Выводить текст ошибки в браузер при переходе из МИС в МР

--- hippocrates/blueprints/risar/views/html_integration.py
+++ hippocrates/blueprints/risar/views/html_integration.py
@@ -10,6 +10,8 @@ import datetime
 from contextlib import contextmanager
 
 import os
+
+import flask
 import requests
 from flask import url_for, redirect
 from hippocrates.blueprints.risar.views.api.integration.card.api import \
@@ -32,65 +34,69 @@ logger = logging.getLogger('simple')
 # /risar/integration/0/remote_entity/tambov/patient/6EHBT7UCLVXB5LSV/inspection.html
 @module.route('/integration/<int:api_version>/remote_entity/<region>/<entity>/<remote_id>/inspection.html')
 def api_card_by_remote_id(api_version, region, entity, remote_id):
-    main_user = current_user.get_main_user()
-    # doctor_code = main_user.regionalCode
-    # org_code = Organisation.query.filter(
-    #     Organisation.id == main_user.org_id
-    # ).value(Organisation.regionalCode)
-    # по разным причинам бывают в main_user не те коды, или их нет
-    person = Person.query.get(main_user.id)
-    doctor_code = person.regionalCode
-    org_code = Organisation.query.filter(
-        Organisation.id == person.org_id
-    ).value(Organisation.regionalCode)
+    try:
+        main_user = current_user.get_main_user()
+        # doctor_code = main_user.regionalCode
+        # org_code = Organisation.query.filter(
+        #     Organisation.id == main_user.org_id
+        # ).value(Organisation.regionalCode)
+        # по разным причинам бывают в main_user не те коды, или их нет
+        person = Person.query.get(main_user.id)
+        doctor_code = person.regionalCode
+        org_code = Organisation.query.filter(
+            Organisation.id == person.org_id
+        ).value(Organisation.regionalCode)
 
-    # если глюк оказался вдруг
-    # doctor_code = '58211165'  # Коняев /Тамбов
-    # org_code = '1434663'  # Контрольная МО /Тамбов
+        # если глюк оказался вдруг
+        # doctor_code = '58211165'  # Коняев /Тамбов
+        # org_code = '1434663'  # Контрольная МО /Тамбов
 
-    if not doctor_code or not org_code:
-        err_txt = (
-            u'Не найден Пользователь или Организация (id="%s" doctor_code="%s" org_code="%s")'
-            % (main_user.id, doctor_code, org_code)
-        )
-        logger.error(err_txt)
-        raise Exception(err_txt.encode('utf-8'))
+        if not doctor_code or not org_code:
+            err_txt = (
+                u'Не найден Пользователь или Организация (id="%s" doctor_code="%s" org_code="%s")'
+                % (main_user.id, doctor_code, org_code)
+            )
+            logger.error(err_txt)
+            raise Exception(err_txt.encode('utf-8'))
 
-    # Добавляем/обновляем пациента по UID РМИС
-    sirius.update_entity_from_mis(region, entity, remote_id)
-    # Запрашиваем ID МР по UID РМИС
-    client_id = sirius.get_risar_id_by_mis_id(region, entity, remote_id)
+        # Добавляем/обновляем пациента по UID РМИС
+        sirius.update_entity_from_mis(region, entity, remote_id)
+        # Запрашиваем ID МР по UID РМИС
+        client_id = sirius.get_risar_id_by_mis_id(region, entity, remote_id)
 
-    # ищем первую открытую карту пациента
-    cards = Event.query.join(EventType, rbRequestType).filter(
-        Event.deleted == 0,
-        rbRequestType.code == request_type_pregnancy,
-        Event.client_id == client_id,
-        Event.execDate == None,
-    ).order_by(Event.createDatetime.desc()).all()
-    card_id = None
-    if cards:
-        for card in cards:
-            if not card.is_closed:  # учитывает, например, +2 дня, рез. обращения
-                card_id = card.id
-                break
-    # если нет открытой карты для пациента
-    if not card_id and client_id:
-        # создаем в рисар новую карту
-        data = {
-            'client_id': client_id,
-            'card_set_date': datetime.date.today().isoformat(),
-            'card_doctor': doctor_code,
-            'card_LPU': org_code,
-        }
-        card_id = create_or_update_card(data, True, api_version, card_id)
-        # todo возникает проблема привязки к пациенту из-за уровня изоляции
-        # протестить вариант
-        # db.session.commit()
-        # card_id = card_save_or_update_get_id(data, True, api_version)
+        # ищем первую открытую карту пациента
+        cards = Event.query.join(EventType, rbRequestType).filter(
+            Event.deleted == 0,
+            rbRequestType.code == request_type_pregnancy,
+            Event.client_id == client_id,
+            Event.execDate == None,
+        ).order_by(Event.createDatetime.desc()).all()
+        card_id = None
+        if cards:
+            for card in cards:
+                if not card.is_closed:  # учитывает, например, +2 дня, рез. обращения
+                    card_id = card.id
+                    break
+        # если нет открытой карты для пациента
+        if not card_id and client_id:
+            # создаем в рисар новую карту
+            data = {
+                'client_id': client_id,
+                'card_set_date': datetime.date.today().isoformat(),
+                'card_doctor': doctor_code,
+                'card_LPU': org_code,
+            }
+            card_id = create_or_update_card(data, True, api_version, card_id)
+            # todo возникает проблема привязки к пациенту из-за уровня изоляции
+            # протестить вариант
+            # db.session.commit()
+            # card_id = card_save_or_update_get_id(data, True, api_version)
 
-        # запись ID карты в шину
-        sirius.save_card_ids_match(card_id, region, entity, remote_id)
+            # запись ID карты в шину
+            sirius.save_card_ids_match(card_id, region, entity, remote_id)
+    except Exception as exc:
+        text = exc.message
+        return flask.Response(text)
 
     # переход на страницу осмотра пациента по ID карты
     return redirect(url_for('.html_inspection', event_id=card_id))
