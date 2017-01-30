5947565aa2e155cf526aa96b8c9c5f230fe1bd4c
Dmitry Paschenko <paschenko@bars-open.ru>
2017-01-30 22:02:02 +0300

    RIMIS-2009 Тула. Расписание. Интеграция. Типы резерва, внеплановые записи

--- hippocrates/blueprints/risar/views/api/integration/schedule/xform.py
+++ hippocrates/blueprints/risar/views/api/integration/schedule/xform.py
@@ -13,7 +13,8 @@ from nemesis.models.schedule import ScheduleTicket, Schedule, rbReceptionType, r
     ScheduleClientTicket
 from nemesis.models.person import Person
 from nemesis.systemwide import db
-from nemesis.lib.utils import safe_date, format_time, safe_dict, safe_time
+from nemesis.lib.utils import safe_date, format_time, safe_dict, safe_time, safe_int, \
+    safe_bool
 from nemesis.lib.apiutils import ApiException
 
 
@@ -231,20 +232,25 @@ class ScheduleFullXForm(ScheduleFullSchema, XForm):
     def convert_and_format(self, data):
         res = {}
         person = self.find_doctor(data['doctor'], data['hospital'])
+        reserve_type_rb = self.rb(data.get('quota_type'), 'rbReserveType') or {}
         res.update({
-            'schedule_ticket_id': data.get('schedule_ticket_id'),
+            'schedule_id': data.get('schedule_id'),
             'person': person,
             'date': safe_date(data['date']),
             'time_begin': safe_time(data['time_begin']),
             'time_end': safe_time(data['time_end']),
-            'quantity': data.get('quantity')
+            'quantity': data.get('quantity'),
+            'reserve_type_id': reserve_type_rb.get('id'),
+            'appointment_permited': int(safe_bool(data.get('appointment_permited'))),
         })
         sts = []
         for st_data in data.get('schedule_tickets', []):
             st = {
                 'time_begin': safe_time(st_data['time_begin']),
                 'time_end': safe_time(st_data['time_end']),
-                'patient': None
+                'patient': None,
+                'schedule_ticket_type': safe_int(st_data.get('schedule_ticket_type')) or 1,
+                'schedule_ticket_id': safe_int(st_data.get('schedule_ticket_id')),
             }
             if 'patient' in st_data:
                 st['patient'] = self.find_client(st_data['patient'])
@@ -258,12 +264,24 @@ class ScheduleFullXForm(ScheduleFullSchema, XForm):
         s = Schedule(
             person=data['person'], date=data['date'],
             begTime=data['time_begin'], endTime=data['time_end'],
-            numTickets=data['quantity'], receptionType=rec_type
+            numTickets=data['quantity'], receptionType=rec_type,
+            reserve_type_id=data['reserve_type_id'],
+            appointment_permitted=data['appointment_permited'],
         )
 
-        attendance = rbAttendanceType.cache().by_code()[u'planned']
         # make interval slots
         for st_data in data['schedule_tickets']:
+            att_code = u'extra' if st_data['schedule_ticket_type'] else u'planned'
+            if att_code == u'planned':
+                if not (st_data['time_begin'] and st_data['time_end']):
+                    raise ApiException(
+                        VALIDATION_ERROR,
+                        u'Не указано время начала или окончания слота'
+                        u' time_begin=(%s) time_end=(%s) schedule_ticket_id=(%s)' %
+                        (st_data['time_begin'], st_data['time_end'],
+                         st_data['schedule_ticket_id'])
+                    )
+            attendance = rbAttendanceType.cache().by_code()[att_code]
             st = ScheduleTicket(
                 begTime=st_data['time_begin'], endTime=st_data['time_end'],
                 attendanceType=attendance
@@ -355,6 +373,7 @@ class ScheduleFullXForm(ScheduleFullSchema, XForm):
     def _represent_intervals(self, schedule):
         return [
             {
+                'schedule_ticket_id': st.id,
                 'time_begin': format_time(st.begTime),
                 'time_end': format_time(st.begTime),
                 'patient': self.or_undefined(st.client_ticket and st.client_ticket.client_id),
--- hippocrates/blueprints/risar/views/api/integration/schedule_tickets/schemas.py
+++ hippocrates/blueprints/risar/views/api/integration/schedule_tickets/schemas.py
@@ -49,9 +49,13 @@ class ScheduleTicketsSchema(Schema):
                     "description": "Время приема",
                     "type": "string",
                     "pattern": "^([0-9]|0[0-9]|1[0-9]|2[0-3]):([0-5][0-9])$"
+                },
+                "schedule_ticket_type": {
+                    "description": "Тип записи на прием",
+                    "type": "string"
                 }
             },
-            "required": ["schedule_ticket_id","hospital","doctor","patient","date","time_begin","time_end"]
+            "required": ["schedule_ticket_id","hospital","doctor","patient","date","time_begin","time_end","schedule_ticket_type"]
         },
         "minItems": 0
     }]
@@ -97,7 +101,11 @@ class ScheduleTicketSchema(Schema):
                 "description": "Время приема",
                 "type": "string",
                 "pattern": "^([0-9]|0[0-9]|1[0-9]|2[0-3]):([0-5][0-9])$"
+            },
+            "schedule_ticket_type": {
+                "description": "Тип записи на прием",
+                "type": "string"
             }
         },
-        "required": ["hospital","doctor","patient","date","time_begin","time_end"]
+        "required": ["hospital","doctor","patient","date","schedule_ticket_type"]
     }]
