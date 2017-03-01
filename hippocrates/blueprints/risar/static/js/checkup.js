dbc55e585954cfd86640bc4b01e300ce188ac51f
Aleksandr Krivopustov <a.krivopustov@bars-open.ru>
2017-03-01 17:08:08 +0300

    RIMIS-2114 Доработать форму отображения информации в меню "Плод"(данные ктг)

--- hippocrates/blueprints/risar/static/js/checkup.js
+++ hippocrates/blueprints/risar/static/js/checkup.js
@@ -277,4 +277,39 @@ WebMis20.controller('BasePregCheckupWatchesCtrl', ['$scope', '$filter', function
         }
    }, true);
 
+}])
+.controller('CheckupBaseChildCtrl', ['$scope', function ($scope) {
+    function cleanPhisher() {
+        $scope.child.state.basal = null;
+        $scope.child.state.variability_range = null;
+        $scope.child.state.frequency_per_minute = null;
+        $scope.child.state.acceleration = null;
+        $scope.child.state.deceleration = null;
+        $scope.child.state.fisher_ktg_rate = null;
+    }
+    function cleanStv() {
+        $scope.child.state.stv_evaluation = null;
+    }
+    $scope.$watch('child.state.ktg_input', function (n, o) {
+        if (n === 'fisher') {
+            $scope.isPhisher = true;
+            $scope.isStv = false;
+            cleanStv();
+        } else if (n === 'stv') {
+            $scope.isStv = true;
+            $scope.isPhisher = false;
+            cleanPhisher();
+        } else {
+          $scope.isPhisher = false;
+          $scope.isStv = false;
+        }
+    }, true);
+    $scope.$watch('child.state.stv_evaluation', function (n, o) {
+        if (n) {
+            $scope.stvDescription = n <= 4 ? 'патология' : 'норма';
+        } else {
+            $scope.stvDescription = null;
+        }
+    }, true);
+    
 }]);
