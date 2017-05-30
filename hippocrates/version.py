e9d2a51dd6aba769c8bb1114529902b428d16990
Oleg Tkachenko <o.tkachenko@bars-open.ru>
2017-05-30 12:19:21 +0200

    Version 1.4.37

--- hippocrates/version.py
+++ hippocrates/version.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
-version = u'1.4.36'
-last_change_date = datetime(year=2017, month=4, day=24, hour=15)
+version = u'1.4.37'
+last_change_date = datetime(year=2017, month=5, day=30, hour=12)
